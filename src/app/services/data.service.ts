3420639bdbfb59203b8a2c34bbbe8cbec2dba55a
unknown <makprg@ukr.net>
2020-07-31 19:35:23 +0300

    checked

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,7 +5,7 @@ export interface Message {
   subject: string;
   date: string;
   id: number;
-  read: boolean;
+  done: boolean;
 }
 
 @Injectable({
@@ -18,56 +18,56 @@ export class DataService {
       subject: 'New event: Trip to Vegas',
       date: '9:32 AM',
       id: 0,
-      read: false
+      done: true
     },
     {
       fromName: 'Lauren Ruthford',
       subject: 'Long time no chat',
       date: '6:12 AM',
       id: 1,
-      read: false
+      done: false
     },
     {
       fromName: 'Jordan Firth',
       subject: 'Report Results',
       date: '4:55 AM',
       id: 2,
-      read: false
+      done: false
     },
     {
       fromName: 'Bill Thomas',
       subject: 'The situation',
       date: 'Yesterday',
       id: 3,
-      read: false
+      done: false
     },
     {
       fromName: 'Joanne Pollan',
       subject: 'Updated invitation: Swim lessons',
       date: 'Yesterday',
       id: 4,
-      read: false
+      done: true
     },
     {
       fromName: 'Andrea Cornerston',
       subject: 'Last minute ask',
       date: 'Yesterday',
       id: 5,
-      read: false
+      done: false
     },
     {
       fromName: 'Moe Chamont',
       subject: 'Family Calendar - Version 1',
       date: 'Last Week',
       id: 6,
-      read: false
+      done: false
     },
     {
       fromName: 'Kelly Richardson',
       subject: 'Placeholder Headhots',
       date: 'Last Week',
       id: 7,
-      read: false
+      done: true
     }
   ];
 
